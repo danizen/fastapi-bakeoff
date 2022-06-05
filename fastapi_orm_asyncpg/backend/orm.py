23638ec1e08f792f9dcd1cbc8cd2158bd788c357
Dan Davis <dan@danizen.net>
2022-06-05 09:40:55 -0400

    Simplify and combine the database and orm files.

--- fastapi_orm_asyncpg/backend/orm.py
+++ fastapi_orm_asyncpg/backend/orm.py
@@ -9,21 +9,21 @@ from sqlalchemy.orm import (
     relationship
 )
 
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext import asyncio as sqlalchemy_async
 
 from .config import Settings
 
 
-def create_engine(settings: Settings):
-    dsn_format = 'postgresql+asyncpg://{user}:{password}@{host}:{port}/{name}'
-    dsn = dsn_format.format(
+def create_async_engine(settings: Settings):
+    dsn = 'postgresql+asyncpg://{user}:{password}@{host}:{port}/{name}'
+    dsn = dsn.format(
         user=settings.db_user,
         password=settings.db_password,
         host=settings.db_host,
         port=settings.db_port,
         name=settings.db_name
     )
-    return create_async_engine(
+    return sqlalchemy_async.create_async_engine(
         dsn,
         echo=settings.echo_sql,
         # pool_size=settings.pool_min_size,

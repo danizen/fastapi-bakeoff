82ffa07e67352add404b044023eb73a7401241f2
Dan Davis <dansmood@gmail.com>
2022-09-30 10:51:18 -0400

    VPC template not complete, but working.

--- templates/vpc/vpc.tf
+++ templates/vpc/vpc.tf
@@ -1,6 +1,9 @@
 resource "aws_vpc" "vpc" {
   cidr_block = var.vpc_cidr
 
+  enable_dns_support = true
+  enable_dns_hostnames = true
+
   tags = {
     Name = var.vpc_name
   }
@@ -35,8 +38,9 @@ resource "aws_egress_only_internet_gateway" "gw" {
 #   vpc_id              = aws_vpc.vpc.id
 # }
 
-resource "aws_route_table" "rt" {
-  vpc_id = aws_vpc.vpc.id
+# route table without subnets
+resource "aws_default_route_table" "rt" {
+  default_route_table_id = aws_vpc.vpc.default_route_table_id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -50,5 +54,23 @@ resource "aws_route_table" "rt" {
 
   tags = {
     Name = "${var.vpc_name}-rtb"
+  }
+}
+
+# network acl
+resource "aws_default_network_acl" "acl" {
+  default_network_acl_id = aws_vpc.vpc.default_network_acl_id
+
+  ingress {
+    protocol   = -1
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = aws_vpc.vpc.cidr_block
+    from_port  = 0
+    to_port    = 0
+  }
+
+  tags = {
+    Name = "${var.vpc_name}-acl"
   }
 }
